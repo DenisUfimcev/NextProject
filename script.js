d5e2f5fb1fa56b9c8e9e55a5406b18e4b4a373b7
Lord <ITDenisUF@gmail.com>
2023-05-07 00:01:08 +0300

    test commit

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+const lines = 5;
+let result = '';
+let stars = 1;
+// Проверяется именно переменная result, формируйте строку в ней
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += ' '
+    }
+    for (let j = 0; j < stars; j++) {
+        result += '*'
+    }
+    stars += 2
+    result += `\n`
+}
 
-let check = 5;
+console.log(result)
 
-function check() {
-    if (confirm('Do you got it?')) {
-        return 'You agreed'
-    } 
-    return 'You refuse, sry'
-}adsa
-Here is too
-Test third commit
+
