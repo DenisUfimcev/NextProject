4d9486689a348eb8fbc4d11d2dd23184057f1eeb
Lord <ITDenisUF@gmail.com>
2023-05-07 00:10:34 +0300

    test commit_10

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const lines = 5;
 let result = '';
-let stars = 1;
-// Проверяется именно переменная result, формируйте строку в ней
-for (let i = 0; i <= lines; i++) {
+let stars = 1;fdsf
+// Проверяется именно переменная result, формируйте строку sdfв ней
+for (let i = 0; i <= lines; i++) {dsf
     for (let j = 0; j < lines - i; j++) {
         result += ' '
-    }
+    }sdfdsf
     for (let j = 0; j < stars; j++) {
         result += '*'
     }
-    stars += 2
+    stars += 2dslfnldsnfldsf
     result += `\n`
 }
 
